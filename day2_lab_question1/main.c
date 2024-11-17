7918b48f574e6ffc7717ad79d64cd9c11b88bbc4
NouranMohamedFouad <nouranloly@gmail.com>
2024-11-18 01:52:33 +0200

    Q 1 modified

--- day2_lab_question1/main.c
+++ day2_lab_question1/main.c
@@ -5,11 +5,14 @@ int main()
 {
     // constant for each row,col,and diagonal = n*(n^2 +1)/2
     // if n=3 ,so the matrix will contain numbers 1...9
-    //rule: number 1 always stored in (n/2,n-1)
     // i moves anti-clock wise , while j moves clock wise
-    printf("please enter the number of the Magic Square: ");
+    printf("please enter the number of the Magic Square(must be odd number): ");
     int num=0;
     scanf("%d",&num);
+    if(num%2==0){
+        printf("sorry this is not an odd number!");
+        return;
+    }
 
     int magicSquare[num][num];
 
@@ -24,70 +27,49 @@ int main()
 
     ////////////////////////////////////////////////////
 
-    //first value always to be (num/2,num-1)
-    int row=num/2;
-    int column=num-1;
+    int row=0;
+    int column=num/2;
     magicSquare[row][column]=1;
 
     //starting from al 2nd number
-    int counter=2;
-    i=1;
-    while(i<num*num)
+
+    i=2;
+    while(i<=num*num)
     {
         row-=1;
         column+=1;
-        while(magicSquare[row][column]!= 0)
+
+        if(row<= -1 && column>=num)
         {
-            row+=1;
-            column-=2;
-            if(row== -1)
-            {
-                row=num-1;
-            }
-            if(column==num)
-            {
-                column=0;
-            }
-            if(row== -1 && column==num)
-            {
-                row=0;
-                column=num-2;
-            }
+            row+=2;
+            column--;
         }
-        if(row== -1)
-        {
-            row=num-1;
-        }
-        if(column==num)
+        else if(column>=num)
         {
             column=0;
         }
-        if(row== -1 && column==num)
+        else if(row<= -1)
         {
-            row=0;
-            column=num-2;
+            row=num-1;
         }
-        if(magicSquare[row][column]== 0)
+        if(magicSquare[row][column]!= 0)
         {
-            magicSquare[row][column]=counter++;
+            row+=2;
+            column-=1;
         }
+        magicSquare[row][column]=i;
+
         i++;
     }
-
 
     i=0,j=0;
     for(i=0;i<num;i++)
     {
         for(j=0;j<num;j++)
         {
-            printf("num %d %d  ",i,j);
-
-
             printf("%d \t",magicSquare[i][j]);
         }
         printf("\n");
-
     }
-
     return 0;
 }
